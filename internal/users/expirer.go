dce38afbffd835df987f033682280bf5b7b0f67e
Henry Whitaker <henrywhitaker3@outlook.com>
2025-06-16 21:40:44 +0100

    feat: add expiry worker to delete old refresh tokens
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

---
+++ internal/users/expirer.go
@@ -0,0 +1,35 @@
+package users
+
+import (
+	"context"
+	"time"
+
+	"github.com/henrywhitaker3/go-template/database/queries"
+	"github.com/henrywhitaker3/go-template/internal/workers"
+)
+
+type Expirer struct {
+	db *queries.Queries
+}
+
+func NewExpirer(db *queries.Queries) *Expirer {
+	return &Expirer{
+		db: db,
+	}
+}
+
+func (e *Expirer) Run(ctx context.Context) error {
+	return e.db.DeleteExpiredRefreshTokens(ctx, time.Now().Unix())
+}
+
+func (e *Expirer) Name() string {
+	return "users:expirer"
+}
+
+func (e *Expirer) Timeout() time.Duration {
+	return time.Second * 15
+}
+
+func (e *Expirer) Interval() workers.Interval {
+	return workers.NewInterval(time.Minute)
+}
